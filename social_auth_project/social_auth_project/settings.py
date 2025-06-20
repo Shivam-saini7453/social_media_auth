6d25b546205828b7cac06afd843b7acee1abe692
Shivam-saini7453 <shivasaini7453@gmail.com>
2025-06-20 11:21:58 +0530

    your update code

--- social_auth_project/social_auth_project/settings.py
+++ social_auth_project/social_auth_project/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#=j3nq&n5frlcua^bxl%a3i4k1@bo&7&m2gco6aq*t1wm08mzm'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
