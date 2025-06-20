9d8d8560cd9942d4bfa66948693a415dbcf0ee6c
Shivam-saini7453 <shivasaini7453@gmail.com>
2025-06-20 11:05:25 +0530

    your update code

--- social_auth_project/social_auth_project/settings.py
+++ social_auth_project/social_auth_project/settings.py
@@ -18,7 +18,13 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-#=j3nq&n5frlcua^bxl%a3i4k1@bo&7&m2gco6aq*t1wm08mzm'
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
